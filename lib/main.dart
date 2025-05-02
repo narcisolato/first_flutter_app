f4cfaba09b9384b5327409d306a937d26de25655
trueor <wisdomichanuk@gmail.com>
2025-05-02 15:18:15 +0900

    feat: 헤더 추가

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,45 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('first app')
-        ),
-        body: Center(
-          child: Text('hello flutter'),
+        // appBar: AppBar(
+        //   title: Text('first app')
+        // ),
+        backgroundColor: Color(0xFF181818),
+        // body: Center(
+        //   child: Text('hello flutter'),
+        // ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Chanuk',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back!',
+                        style: TextStyle(
+                          color: Colors.white.withAlpha(196),
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
