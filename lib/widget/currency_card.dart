e13ec8157ef806006938509e723bcbeabac3f8f6
trueor <wisdomichanuk@gmail.com>
2025-05-02 21:12:48 +0900

    feat: 카드 도전 과제 추가

--- lib/widget/currency_card.dart
+++ lib/widget/currency_card.dart
@@ -6,9 +6,11 @@ class CurrencyCard extends StatelessWidget {
   final String amount;
   final IconData icon;
   final bool inverted;
+  final int order;
 
   late final _backgroundColor = inverted ? Color(0xFF1F2123) : Colors.white;
   late final _textColor = inverted ? Colors.white : Color(0xFF1F2123);
+  late final double _offset = order * -20;
 
   CurrencyCard({
     super.key,
@@ -17,59 +19,63 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.icon,
     required this.inverted,
+    required this.order,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: _backgroundColor,
-        borderRadius: BorderRadius.circular(24),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    color: _textColor,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+    return Transform.translate(
+      offset: Offset(0, _offset),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: _backgroundColor,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: _textColor,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 12),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(color: _textColor, fontSize: 18),
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: _textColor.withAlpha(200),
-                        fontSize: 18,
+                  SizedBox(height: 12),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(color: _textColor, fontSize: 18),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
-              scale: 2.2,
-              child: Transform.translate(
-                offset: Offset(-10, 7),
-                child: Icon(icon, color: _textColor, size: 81),
+                      SizedBox(width: 5),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: _textColor.withAlpha(200),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ),
-          ],
+              Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  offset: Offset(-10, 7),
+                  child: Icon(icon, color: _textColor, size: 81),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
